13782fddccfc7f0c1884dd3f16e314e3d624c2b1
MachineMachzor <32687224+MachineMachzor@users.noreply.github.com>
2021-02-14 22:23:03 -0500

    Update alpacaConfig.py

--- alpacaConfig.py
+++ alpacaConfig.py
@@ -6,24 +6,11 @@ baseUrl = 'https://paper-api.alpaca.markets'#endpoint url. It's paper money rn, 
 dbFile = "D:/AppDbPersonal/app.db"
 #Changed dbFile location to D drive because there's more space (Getting historical minute data is a big operation)
 
-EMAIL_ADDRESS = "tradenotifications2@gmail.com"
+EMAIL_ADDRESS = "tradeExampleEmail@gmail.com" #Email address you want to use for 
 EMAIL_HOST = "smtp.gmail.com"#Server host -> specifying gmail host
 EMAIL_PORT = 465 #Default port
-EMAIL_PASSWORD = "Poweryoga1@"
+EMAIL_PASSWORD = "examplePass" #Email password
 
 
-
-MOTLEY_EMAIL = "tannerfesta@gmail.com"
-MOTLEY_PASS = "Poweryoga1@"
-
-
-"""
-context = ssl.create_default_context() #Default context
-                    with smtplib.SMTP_SSL(config.EMAIL_HOST, config.EMAIL_PORT, context=context) as server:
-                        server.login(config.EMAIL_ADDRESS, config.EMAIL_PASSWORD) #Login
-                        email_message = f"Subject: Trade Notifications for {iteratorDate}\n\n" #Our email subject
-                        message = f"{symbol} has dec momentum with span_a {data_market['senkou_span_a'][-1]} span_b {data_market['senkou_span_b'][-1]} and close {data_market['close'][-1]}"
-                        email_message = "\n\n".join(message) #Our message
-                        server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_ADDRESS, email_message) #From, to, message (sending to ourself)
-                        sys.exit(1)
-"""
+MOTLEY_EMAIL = "exampleEmail@gmail.com" #Create a motley fool account, and just input that into here
+MOTLEY_PASS = "examplePass123"
